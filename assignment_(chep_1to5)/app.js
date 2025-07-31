03fc8918ad958ce2fdb1fc698b7cc5f1d90996f6
rabiasaleem2307@gmail.com <aamirsaamia@gmail.com>
2025-07-31 17:29:07 +0500

    first assignment end

---
+++ assignment_(chep_1to5)/app.js
@@ -0,0 +1,25 @@
+// __________________chaopter_1_
+
+// ________________Question_1_
+// alert("welcome to JavaScript class")
+
+// ________________Question_2_
+// alert("Error!Please enter a valid passwerd.")
+
+//  ________________Question_3_
+//  alert("Welcome to JS land..\n happy Coding")
+
+//  ________________Question_4_
+// alert("Welcome to JS land..")
+// alert("Happy Coding")
+
+//  ________________Question_5_
+// console.log("Hello... i can run js through my web browser's console");
+
+//  ________________Question_6_ 
+// indexfile me hy
+//  alert("Welcome to JS land..\n happy Coding")
+ 
+
+
+
