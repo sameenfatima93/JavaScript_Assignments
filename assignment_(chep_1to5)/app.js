f1fafca64c5ea9bbabe5aae7987c4c7a1fd74841
rabiasaleem2307@gmail.com <aamirsaamia@gmail.com>
2025-07-31 17:53:08 +0500

    chapter_2 done

--- assignment_(chep_1to5)/app.js
+++ assignment_(chep_1to5)/app.js
@@ -20,6 +20,57 @@
 // indexfile me hy
 //  alert("Welcome to JS land..\n happy Coding")
  
+//_____***chapter_1_END***___________//
+
+// __________________chaopter_2_
+
+//  ________________Question_1_ 
+// var userNmae = ""
+
+//  ________________Question_2_ 
+//  var myNmae = "Sameen"
+
+//  ________________Question_3_ 
+//  var massage = "Hello world"
+//  alert(massage)
+
+//  ________________Question_4_ 
+// var studentName = "jhone Doe"
+// alert(studentName)
+// var studentAge = "15 years old"
+// alert(studentAge)
+// var studentEducation = "Certified mobile Application Development"
+//  alert(studentEducation)
+
+//  ________________Question_5_ 
+// alert("Pizza \n Pizz \n Piz \n Pi \n P")
+ 
+//  ________________Question_6_ 
+// var email = "example@gmail.com"
+// alert("my email is " + email)
+
+//  ________________Question_7_
+// var book = "Asmarter way to learn JavaScript" 
+// alert("i am trying to learn the book" +" "+book)
+
+//  ________________Question_8_
+// document.write("yah ! i can write HTML content through JavaScript")
+
+//  ________________Question_9_
+// var design = " “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” "
+// alert(design)
+
+// document.write("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+
+
+
+
+
+
+
+
+
 
 
 
