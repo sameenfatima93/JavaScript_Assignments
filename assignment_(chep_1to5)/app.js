548203b886ed52d844c7fbd3bde6ca125c564cf3
SameenFatima <sameen.fatima993@gmail.com>
2025-08-01 08:49:43 +0500

    assignment1Done

--- assignment_(chep_1to5)/app.js
+++ assignment_(chep_1to5)/app.js
@@ -19,22 +19,26 @@
 //  ________________Question_6_ 
 // indexfile me hy
 //  alert("Welcome to JS land..\n happy Coding")
- 
+
+//////////////////////////////////////////
 //_____***chapter_1_END***___________//
+///////////////////////////////////////////
+
+
 
 // __________________chaopter_2_
 
-//  ________________Question_1_ 
+//  _________Question_1_ 
 // var userNmae = ""
 
-//  ________________Question_2_ 
+//  _________Question_2_ 
 //  var myNmae = "Sameen"
 
-//  ________________Question_3_ 
+//  __________Question_3_ 
 //  var massage = "Hello world"
 //  alert(massage)
 
-//  ________________Question_4_ 
+//  __________Question_4_ 
 // var studentName = "jhone Doe"
 // alert(studentName)
 // var studentAge = "15 years old"
@@ -42,27 +46,149 @@
 // var studentEducation = "Certified mobile Application Development"
 //  alert(studentEducation)
 
-//  ________________Question_5_ 
+//  __________Question_5_ 
 // alert("Pizza \n Pizz \n Piz \n Pi \n P")
  
-//  ________________Question_6_ 
+//  __________Question_6_ 
 // var email = "example@gmail.com"
 // alert("my email is " + email)
 
-//  ________________Question_7_
+//  ___________Question_7_
 // var book = "Asmarter way to learn JavaScript" 
 // alert("i am trying to learn the book" +" "+book)
 
-//  ________________Question_8_
+//  ___________Question_8_
 // document.write("yah ! i can write HTML content through JavaScript")
 
-//  ________________Question_9_
+//  ____________Question_9_
 // var design = " “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” "
 // alert(design)
 
 // document.write("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
 
+//////////////////////////////////////////
+//_____***chapter_2_END***___________//
+///////////////////////////////////////////
 
+// __________________chaopter_3_
+
+// ____________Question_1_
+// var age = 20
+// alert("i am " +" "+ age + " "+ "years old" )
+
+// ____________Question_2_
+// var visitedTime = "N"
+// visitedTime = "10time"
+// alert("you visited this site " +""+ visitedTime)
+
+// ____________Question_3_
+// var birthYear = "1999"
+// document.write("My birth year is "+""+ birthYear + " \n " + "" + "Data type of my declared Variable is number");
+
+// ____________Question_4_
+// var visiterName = "John Doe"
+// var product = "T Shirt"
+// var quantity = 5
+// document.write(`mr ${visiterName} orderd ${quantity} ${product} on XYZ Clothing store`)
+
+//////////////////////////////////////////
+//_____***chapter_3_END***___________//
+///////////////////////////////////////////
+
+
+// __________________chaopter_4_
+
+// ____________Question_1_
+// var courses = {
+//     eng: "English",
+//     urdu: "Urdu",
+//     math: "Mathematics"
+// };
+// console.log(courses);
+// this is and object methode//
+
+// ____________Question_2_
+// legal variables
+// 1) var myName
+// 2) var studenName
+// 3) var theBookWriter
+// 4) var slectedCourse
+// 5) var emailAdd
+
+// illegal variables
+// 1) var var =
+// 2) var 10years =
+// 3) var "name" =
+// 4) var function =
+// 5) var mathode =
+
+// ____________Question_3_
+
+// not solved
+
+
+//////////////////////////////////////////
+////_____***chapter_4_END***_______//////
+//////////////////////////////////////////
+
+// __________________chaopter_5_
+
+//_______question_1_
+
+// var num1 = 3
+// var num2 = 5
+// document.write(`sum of  ${num1} + ${num2} is 8`)
+
+//_______question_2_
+
+// var num1 = 10
+// var num2 = 5
+// document.write(`subtraction of  ${num1} - ${num2} is= 5`)
+
+//  var num1 = 3
+//  var num2 = 5
+//  document.write(`multiplication of  ${num1} * ${num2} is =15`)
+
+//  var num1 = 10
+//  var num2 = 2
+//  document.write(`division of  ${num1} / ${num2} is 5`)
+
+// var num1 = 5
+// var num2 = 5
+// document.write(`remender of  ${num1} % ${num2} is 0 `)
+
+//_______question_3_
+ 
+    // var value;
+    // document.write("<p>Value after variable declaration is: " + value + "</p>");
+
+    // value = 5;
+    // document.write("<p>Initial value: " + value + "</p>");
+
+    // value++;
+    // document.write("<p>Value after increment is: " + value + "</p>");
+
+    // value = value + 7;
+    // document.write("<p>Value after addition is: " + value + "</p>");
+
+    // value--;
+    // document.write("<p>Value after decrement is: " + value + "</p>");
+
+    // var remainder = value % 3;
+    // document.write("<p>The remainder is: " + remainder + "</p>");
+    
+    //________question_4_
+
+    // var costTicket = 600
+    // var soldTickets = 5
+    // var sumOfTotalPrice = costTicket * soldTickets
+
+    // document.write(`total cost of to by ${soldTickets} Tickets to a movie  is ${sumOfTotalPrice} PKR`)
+    
+    //_______qustion_5_to_13
+    //  abhi nhi parhy 
+
+    
 
 
 
