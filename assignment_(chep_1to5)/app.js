7167769558e80d2e442ef0c6805813311ee73c09
SameenFatima <sameen.fatima993@gmail.com>
2025-08-02 01:29:41 +0500

    assignment_1is compelte

--- assignment_(chep_1to5)/app.js
+++ assignment_(chep_1to5)/app.js
@@ -48,7 +48,7 @@
 
 //  __________Question_5_ 
 // alert("Pizza \n Pizz \n Piz \n Pi \n P")
- 
+
 //  __________Question_6_ 
 // var email = "example@gmail.com"
 // alert("my email is " + email)
@@ -158,45 +158,121 @@
 // document.write(`remender of  ${num1} % ${num2} is 0 `)
 
 //_______question_3_
- 
-    // var value;
-    // document.write("<p>Value after variable declaration is: " + value + "</p>");
 
-    // value = 5;
-    // document.write("<p>Initial value: " + value + "</p>");
+// var value;
+// document.write("<p>Value after variable declaration is: " + value + "</p>");
 
-    // value++;
-    // document.write("<p>Value after increment is: " + value + "</p>");
+// value = 5;
+// document.write("<p>Initial value: " + value + "</p>");
 
-    // value = value + 7;
-    // document.write("<p>Value after addition is: " + value + "</p>");
+// value++;
+// document.write("<p>Value after increment is: " + value + "</p>");
 
-    // value--;
-    // document.write("<p>Value after decrement is: " + value + "</p>");
+// value = value + 7;
+// document.write("<p>Value after addition is: " + value + "</p>");
 
-    // var remainder = value % 3;
-    // document.write("<p>The remainder is: " + remainder + "</p>");
-    
-    //________question_4_
+// value--;
+// document.write("<p>Value after decrement is: " + value + "</p>");
 
-    // var costTicket = 600
-    // var soldTickets = 5
-    // var sumOfTotalPrice = costTicket * soldTickets
+// var remainder = value % 3;
+// document.write("<p>The remainder is: " + remainder + "</p>");
 
-    // document.write(`total cost of to by ${soldTickets} Tickets to a movie  is ${sumOfTotalPrice} PKR`)
-    
-    //_______qustion_5_to_13
-    //  abhi nhi parhy 
+//________question_4_
 
-    
+// var costTicket = 600
+// var soldTickets = 5
+// var sumOfTotalPrice = costTicket * soldTickets
+
+// document.write(`total cost of to by ${soldTickets} Tickets to a movie  is ${sumOfTotalPrice} PKR`)
 
 
+//_______qustion_5_
+// document.write("<h1>thable of 4</h1>")
+//  var num = 4
+//  document.write(num + " X 1 =" +num*1 + "<br>")
+//  document.write(num + " X 2 =" +num*2 + "<br>")
+//  document.write(num + " X 3 =" +num*3 + "<br>")
+//  document.write(num + " X 4 =" +num*4 + "<br>")
+//  document.write(num + " X 5 =" +num*5 + "<br>")
+//  document.write(num + " X 6=" +num*6 + "<br>")
+//  document.write(num + " X 7 =" +num*7 + "<br>")
+//  document.write(num + " X 8 =" +num*8 + "<br>")
+//  document.write(num + " X 9 =" +num*9 + "<br>")
+//  document.write(num + " X 10 =" +num*10 + "<br>")
 
+//_______qustion _6__
 
+// var calcius = 25
+// var fahren = 70
+// formula1 = (fahren - 32) * 5/9
+// formula2 =(calcius * 9/5) +32
+// document.write(fahren + "<sup>0</sup>C is" + formula2 + " <sup>0</sup>F" + "<br>")
+// document.write(calcius + "<sup>0</sup>F is" + formula1 + " <sup>0</sup>c" + "<br>")
 
+//______question_7_
 
+// var priceItem1 = 650;
+// var priceItem2 = 100;
+// var quantityItem1 = 3;
+// var quantityItem2 = 7;
+// var shippingCharges = 100;
 
+// var totalItem1 = priceItem1 * quantityItem1;
+// var totalItem2 = priceItem2 * quantityItem2;
+// var totalCost = totalItem1 + totalItem2 + shippingCharges;
+// document.write("<p>Price of item 1 is: " + priceItem1 + "</p>");
+// document.write("<p>Quantity of item 1 is: " + quantityItem1 + "</p>");
+// document.write("<p>Price of item 2 is: " + priceItem2 + "</p>");
+// document.write("<p>Quantity of item 2 is: " + quantityItem2 + "</p>");
+// document.write("<p>Shipping Charges: " + shippingCharges + "</p><hr>");
+// document.write("<h3>Total Cost: " + totalCost + "</h3>");
 
+//________question__8__
 
+//  var totalMarks = 890
+//  var MarksObtained = 804
+//  var percentage = (totalMarks / MarksObtained)*100
+//  document.write("Total Marks:" + totalMarks +"<br>")
+//  document.write("Marks Obtained:" + MarksObtained +"<br>")
+// document.write("<p>Percentage: " + percentage.toFixed(2) + "%</p>");
 
+//_______question__9__
+// var totalPKR = (10 * 104.80) + (25 * 28); 
+// document.write("Total Currency in PKR: " + totalPKR);
+
+//_______question__10__
+// var sum = 5
+// console.log((sum + 5 )* 10 / 2);
+// or
+// var number = 5;
+// var result = ((number + 5) * 10) / 2;
+// document.write("Result: " + result);
+
+// ________question__11__
+// document.write("<h3> the age caculations </h3>")
+// var currentYear = 2025
+// var birthYear = 1993
+// var age1 = currentYear - birthYear
+// var age2 = age1 - 1;
+// document.write(`they are either ${age1} or ${age2} years old`)
+
+//________question__12__
+
+// var radius = 20;
+// var circumference = 2 * 3.142 * radius;
+// var area = 3.142 * radius * radius;
+// document.write("Radius of circle: " + radius + " cm<br>");
+// document.write("The circumference is: " + circumference + " cm<br>");
+// document.write("The area is: " + area + " cm²");
+
+// _____________question__13___
+    // var snack = "Chips";
+    // var currentAge = 25;
+    // var maxAge = 80;
+    // var perDay = 2;
+    // var yearsLeft = maxAge - currentAge;
+    // var totalNeeded = yearsLeft * 365 * perDay;
+    // document.write(
+    //   `You will need ${totalNeeded} ${snack} to last you until the ripe old age of ${maxAge}.`
+    // );
 
