d58d92e8e01845709450664e528e5979db08a620
SameenFatima <sameen.fatima993@gmail.com>
2025-08-08 15:03:09 +0500

    half_done

--- assignment_(chep_9_to_11)/app.js
+++ assignment_(chep_9_to_11)/app.js
@@ -19,3 +19,111 @@
 // }else{
 //      document.write("good night")
 // }
+
+//_______question_3_________//
+// var signalLight = prompt("Enter the color of traffic signal light")
+// if (signalLight === "red") {
+//     alert("Must Stop")
+    
+// } else if(signalLight === "Yellow"){
+//     alert("Ready to move")
+    
+// } else if(signalLight === "green"){
+// alert("Move now")
+// }else{
+//     alert("Enter the correct color of signal light")
+// }
+
+
+//_______question_4_________//
+
+// var fuel = +prompt("Enter remaining fuel in your car (in litres):");
+
+// if (fuel < 0.25) {
+//     alert("Please refill the fuel in your car");
+// }
+
+//_______question_5_________//
+
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// true
+
+//   var b = 82;  
+//  if (b++ === 83){  
+//  alert("given condition for variable b is true");
+//  }
+//  true
+
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// } 
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+//true
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+//  if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// } 
+// // true
+
+//  if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+// false car is biger ten cat
+
+
+//_______question_6_________//
+
+// var sub1 = +prompt("Enter marks obtained in subject 1:");
+// var sub2 = +prompt("Enter marks obtained in subject 2:");
+// var sub3 = +prompt("Enter marks obtained in subject 3:");
+
+// var totalMarks = +prompt("Enter total marks:");
+
+// var obtainedMarks = sub1 + sub2 + sub3;
+
+// var percentage = (obtainedMarks / totalMarks) * 100;
+
+// document.write("Obtained Marks: " + obtainedMarks + "<br>");
+// document.write("Total Marks: " + totalMarks + "<br>");
+// document.write("Percentage: " + percentage + "%<br>");
+
+// if (percentage >= 80) {
+//     document.write("Grade: A-one"+ "<br>" + "Remarks: Bright and shine");
+// }
+// else if (percentage >= 70) {
+//     document.write("Grade: A" + "<br>" + "Remarks: Excellent");
+// }
+// else if (percentage >= 60) {
+//     document.write("Grade: B" + "<br>" + "Remarks: well done");
+// }
+// else if (percentage >= 50) {
+//     document.write("Grade: C" + "<br>" + "Remarks: Keep it up");
+// }
+// else if (percentage >= 40) {
+//     document.write("Grade: D" + "<br>" + "Remarks: Need to improve");
+// }
+// else {
+//     document.write("Grade: Fail");
+// }
