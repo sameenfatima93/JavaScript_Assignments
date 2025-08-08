9a613fb3dd976eb55fd9762039e1ab25a00ecf96
SameenFatima <sameen.fatima993@gmail.com>
2025-08-08 14:29:48 +0500

    assignment_3

---
+++ assignment_(chep_9_to_11)/app.js
@@ -0,0 +1,21 @@
+//________assignemnt_3_____________///
+
+//_______question_1_________//
+
+// var city = prompt("please Enter your City Name")
+// if (city === "karachi") {
+//     document.write("Welcome to the City of Lights")
+// }else{
+//     alert("sorry better luck to the next time")
+// }
+
+//_______question_2_________//
+
+// var gender = prompt("Enter your Gender")
+// if (gender === "male") {
+//      document.write("good morning Sir")  
+// }else if (gender === "female") {
+//      document.write("good morning ma'am")
+// }else{
+//      document.write("good night")
+// }
