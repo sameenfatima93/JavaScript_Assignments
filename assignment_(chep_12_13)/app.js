7605d3a9acacb5ed8b42e9a480261458d6427b4f
SameenFatima <sameen.fatima993@gmail.com>
2025-08-12 15:03:18 +0500

    4th assignment

---
+++ assignment_(chep_12_13)/app.js
@@ -0,0 +1,83 @@
+//______assignment___4__________________//
+
+//_____question__1__________
+// var char = prompt("Enter a single character:");
+
+// var ascii = char.charCodeAt(0);
+
+// if (ascii >= 48 && ascii <= 57) {
+//     alert("You entered a Number");
+// }
+// else if (ascii >= 65 && ascii <= 90) {
+//     alert("You entered an Uppercase Letter");
+// }
+// else if (ascii >= 97 && ascii <= 122) {
+//     alert("You entered a Lowercase Letter");
+// }
+// else {
+//     alert("You entered some other character");
+// }
+
+//_____question__2__________
+// var int1 = +prompt("Enter the first integer:");
+// var int2 = +prompt("Enter the second integer:");
+// if (int1> int2) {
+//     console.log(int1 + "is greater than  " + int2);
+ 
+// }else if(int2 > int1){
+//     console.log(int2 + "is greater than  " + int1);
+
+// } else{
+
+//     console.log("Both numbers are equal.");
+    
+// }
+
+//_____question__3__________
+// var userNum = +prompt("Enter a number:");
+// if (userNum > 0) {
+//     console.log(userNum + "is positive");
+
+// }else if (userNum < 0) {
+//     console.log(userNum + "is nagitive");
+// } else {
+//     console.log("The number is zero.");
+// }
+
+//_______question__4__________
+// var char = prompt("Enter a single character:").toLowerCase();
+
+// if (char.length === 1) {
+//     if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// } else {
+//     console.log("Please enter only one character.");
+// }
+
+//_______question__5__________
+
+// var correctPassword = "mySecret123";
+// var userPassword = prompt("Enter your password:");
+// if (!userPassword) {
+
+//     console.log("Please enter your password");
+// } else if (userPassword === correctPassword) {
+   
+//     console.log("Correct! The password you entered matches the original password");
+// } else {
+ 
+//     console.log("Incorrect password");
+// }
+
+//_______question__6__________
+ var greeting;
+ var hour = 13;
+if (hour < 18) {
+greeting = "Good day";
+}else{
+greeting = "Good evening";
+}
+console.log(greeting);
