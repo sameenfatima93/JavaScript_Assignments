1f255cc467d9569c32b14d0207a571a6c47299b8
SameenFatima <sameen.fatima993@gmail.com>
2025-09-16 13:07:43 +0500

    assignment_6_done

---
+++ assignment_(chep_17_20)/app.js
@@ -0,0 +1,141 @@
+//_______assignment__6_______________________________________//
+//________question__1________________________________//
+
+// var multiArray = [];
+
+
+// multiArray.push([]);
+// multiArray.push([]);
+
+//____________question___2_________________________//
+
+// var matrix = [
+//     [0, 1, 2, 3]
+// ];
+
+
+// console.log(matrix);
+
+//____________question_3_____________________________________//
+// var number = [1,2,3,4,5,6,7,8,9,10]
+// for (let i = 0; i < number.length; i++) {
+//     console.log(number[i]); 
+    
+// }
+
+//__________question__4____________________________________//
+
+// var tableNumber = +prompt("Enter a number for multiplication table:");
+// var tableLength = +prompt("Enter the length of the table:");
+
+
+// for (var i = 1; i <= tableLength; i++) {
+//     console.log(tableNumber + " x " + i + " = " + (tableNumber * i));
+// }
+
+//__________question____5___________________________________//
+
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+// // Print items using for loop
+// for (var i = 0; i < fruits.length; i++) {
+//     console.log("at the numbers of " +  i  + " is " + fruits[i]);
+// }
+
+//_________question__6__________________________________//
+
+// document.write("<b>Counting:</b><br>");
+// for (var i = 1; i <= 15; i++) {
+//     document.write(i + (i < 15 ? ", " : ""));
+// }
+// document.write("<br><br>");
+
+// document.write("<b>Reverse Counting:</b><br>");
+// for (var i = 10; i >= 1; i--) {
+//     document.write(i + (i > 1 ? ", " : ""));
+// }
+// document.write("<br><br>");
+
+// document.write("<b>Even:</b><br>");
+// for (var i = 0; i <= 20; i += 2) {
+//     document.write(i + (i < 20 ? ", " : ""));
+// }
+// document.write("<br><br>");
+
+// document.write("<b>Odd:</b><br>");
+// for (var i = 1; i < 20; i += 2) {
+//     document.write(i + (i < 19 ? ", " : ""));
+// }
+// document.write("<br><br>");
+
+// document.write("<b>Series:</b><br>");
+// for (var i = 2; i <= 20; i += 2) {
+//     document.write(i + "k" + (i < 20 ? ", " : ""));
+// }
+
+//_________question__7__________________________________//
+
+// var A = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+// var userInput = prompt("Welcome! What do you want to order?").toLowerCase();
+
+// var found = false;
+
+// for (var i = 0; i < A.length; i++) {
+//     if (A[i].toLowerCase() === userInput) {
+//         found = true;
+//         alert(userInput + " is available at index " + i + " in our bakery.");
+//         break;
+//     }
+// }
+// if (!found) {
+//     alert("We are sorry. " + userInput + " is not available in our bakery.");
+// }
+
+//_________question__8___largest number finding_______________________________//
+// Given array
+// var A = [24, 53, 78, 91, 12];
+
+// // Assume first element is largest
+// var largest = A[0];
+
+// // Loop to check all elements
+// for (var i = 1; i < A.length; i++) {
+//     if (A[i] > largest) {
+//         largest = A[i];
+//     }
+// }
+
+// // Show result
+// console.log("The largest number is: " + largest);
+
+//_________question__9___smallest number finding_______________________________//
+// Given array
+// var A = [24, 53, 78, 91, 12];
+
+// // Assume first element is largest
+// var largest = A[0];
+
+// // Loop to check all elements
+// for (var i = 1; i < A.length; i++) {
+//     if (A[i] > largest) {
+//         largest = A[i];
+//     }
+// }
+
+// // Show result
+// console.log("The largest number is: " + largest);
+
+//_________question__9___ranging 5 every 5 rang pr  number finding_______________________________//
+
+// Print multiples of 5 from 1 to 100
+// for (var i = 5; i <= 100; i += 5) {
+//     console.log(i);
+// }
+
+
+
+
+
+
+
